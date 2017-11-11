d3d7f374e9565abe8eca297f8c86a23db3ab8e1c
Francesca Giordano <francesca020406@hotmail.com>
2017-11-11 13:28:31 -0500

    more services, advanced

--- src/app/my-second-counter/my-second-counter.component.ts
+++ src/app/my-second-counter/my-second-counter.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CounterService } from '../services/counter.service';
 
 @Component({
-  selector: 'app-my-second-counter',
-  templateUrl: './my-second-counter.component.html',
-  styleUrls: ['./my-second-counter.component.css'],
-  encapsulation: ViewEncapsulation.None
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.css'],
+  providers: [CounterService]
 })
-export class MySecondCounterComponent implements OnInit {
+export class MyCounterComponent implements OnInit {
+  constructor(private theCounter: CounterService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  incrementCounter() {
+    this.theCounter.increment();
   }
-
 }
